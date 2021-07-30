6914af4313fb1d8f297392f6499f77e5ae2bd982
liamandaidan <liammclaughlinis@gmail.com>
2021-07-30 11:35:53 -0600

    Working copy so far

--- src/sait/mms/application/AppDriver.java
+++ src/sait/mms/application/AppDriver.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import sait.mms.drivers.MariaDBDriver;
+import sait.mms.managers.MovieManagementSystem;
 
 
 /**
@@ -20,16 +21,8 @@ public class AppDriver {
 	 * @throws SQLException 
 	 */
 	public static void main(String[] args) throws SQLException {
-		MariaDBDriver md = new MariaDBDriver();
-		md.connect();
-		String sqlQuery = "Select * from movies";
-		ResultSet results = md.get(sqlQuery);
-		while(results.next()) {
-			int id = results.getInt(1);
-			String name = results.getString(2);
-			System.out.printf("%d, %s\n",id,name);	
-		}
-		md.disconnect();
+		
+		new MovieManagementSystem();
 		
 
  
--- src/sait/mms/managers/MovieManagementSystem.java
+++ src/sait/mms/managers/MovieManagementSystem.java
@@ -3,6 +3,9 @@
  */
 package sait.mms.managers;
 
+import java.sql.*;
+import java.util.*;
+import sait.mms.drivers.MariaDBDriver;
 import sait.mms.problemdomain.Movie;
 
 /**
@@ -11,29 +14,77 @@ import sait.mms.problemdomain.Movie;
  */
 public class MovieManagementSystem {
 
+	private int id;
+	private int duration;
+	private String title;
+	private int year;
+	private MariaDBDriver md;
+	private Scanner in;  
 	
-	public void displayMenu()
-	{
-		
+
+	/**
+	*
+	*
+	*/
+	public MovieManagementSystem() {
+		try {
+			md = new MariaDBDriver();
+			md.connect();
+			displayMenu();
+		} catch (SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+
 	}
-	
-	public void addMovie(Movie theMovie) 
-	{
+
+	public void displayMenu() throws SQLException {
+
 		
+			// exit
+				md.disconnect();	
+		 in = new Scanner(System.in);
+		int option;
+
+		System.out.printf("Jim's Movie Manager%n" + "1. Add New Movie%n" + "2. Print movies released in year%n"
+				+ "3. Print random list of movies%n" + "4. Delete a movie%n" + "5. Exit");
+		//SELECT COUNT(*) FROM movies
+		System.out.printf("Enter an option: ");
+		option = in.nextInt();
+
 	}
+
 	
-	public void printMoviesInYear()
-	{
-		
+
+	/**
+	 * 
+	 * @param theMovie
+	 */
+	public void addMovie(int id, int dur, String title, int yr) {
+		Movie film = new Movie(id, dur, title, yr);
+		String sqlStatement = "INSERT INTO movies(id, duration, title, year) VALUES(" + film.getId() + "," + film.getDuration() + ",'"
+				+ film.getTitle() + "'," + film.getYear() + ");";
+		//int rows = md.update(sqlStatement);
+
 	}
-	
-	public void printRandomMovies()
-	{
-		
+
+	/**
+	 * 
+	 */
+	public void printMoviesInYear() {
+
 	}
-	
-	public void deleteMovie()
-	{
-		
+
+	/**
+	 * 
+	 */
+	public void printRandomMovies() {
+
+	}
+
+	/**
+	 * 
+	 */
+	public void deleteMovie(Movie film) {
+
 	}
 }
