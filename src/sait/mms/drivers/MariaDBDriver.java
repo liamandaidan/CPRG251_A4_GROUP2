452845d3aab9a7a5e9ea25eaa6612e10e0af4db0
liamandaidan <liammclaughlinis@gmail.com>
2021-07-30 10:43:31 -0600

    Maria DB Class

--- src/sait/mms/drivers/MariaDBDriver.java
+++ src/sait/mms/drivers/MariaDBDriver.java
@@ -3,10 +3,54 @@
  */
 package sait.mms.drivers;
 
+import java.sql.*;
+import sait.mms.contracts.DatabaseDriver;
+
 /**
- * @author liamm
+ * @author Ali
  *
  */
-public class MariaDBDriver {
+public class MariaDBDriver implements DatabaseDriver {
+	private static final String SERVER = "localhost";
+	private static final int PORT = 3306;
+	private static final String DATABASE = "cprg251";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "password";
+	Connection connection;
+
+	@Override
+	public void connect() throws SQLException {
+		String dsn = getDsn();
+		connection = DriverManager.getConnection(dsn);
+
+	}
+
+	private String getDsn() {
+		String dsn = String.format("jdbc:mariadb://%s:%d/%s?user=%s&password=%s", SERVER, PORT, DATABASE, USERNAME,
+				PASSWORD);
+		return dsn;
+	}
+
+	@Override
+	public void disconnect() throws SQLException {
+		if (connection != null && !connection.isClosed()) {
+			connection.close();
+		}
+
+	}
+
+	@Override
+	public ResultSet get(String query) throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet results = stmt.executeQuery(query);
+		return results;
+	}
+
+	@Override
+	public int update(String query) throws SQLException {
+		Statement stmt = connection.createStatement();
+		int rows = stmt.executeUpdate(query);
+		return rows;
+	}
 
 }
