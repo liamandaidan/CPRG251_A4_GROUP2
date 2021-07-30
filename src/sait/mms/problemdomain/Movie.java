9363e74d90f6a5b20beb05bc1f9a3a0c2232686f
Mikepcann <mikepcannbass@gmail.com>
2021-07-30 10:49:17 -0600

    Movie class getters and setters

--- src/sait/mms/problemdomain/Movie.java
+++ src/sait/mms/problemdomain/Movie.java
@@ -3,10 +3,113 @@
  */
 package sait.mms.problemdomain;
 
+import java.io.Serializable;
+
 /**
- * @author liamm
+ * @author Mikepcann
  *
  */
-public class Movie {
+public class Movie  implements Serializable{
+	
+	static final long serialVersionUID = 2L;
+	
+	private int id;
+	private int duration;
+	private String title;
+	private int year;
+	
+	/**
+	 *  No arg default constructor. 
+	 */
+	public Movie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	/**
+	 *  Creates a movie object with constructors for the fields.
+	 * @param id Id number of the movie.
+	 * @param duration The duration of the movie.
+	 * @param title The title of the movie.
+	 * @param year The year the movie was released.
+	 */
+	public Movie(int id, int duration, String title, int year) {
+		super();
+		this.id = id;
+		this.duration = duration;
+		this.title = title;
+		this.year = year;
+	}
+
+	/** 
+	 * Gets the ID of the movie.
+	 * @return the id.
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/** Sets the ID of the movie.
+	 * @param id the id to set.
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 *  Gets the duration of the movie.
+	 * @return the duration.
+	 */
+	public int getDuration() {
+		return duration;
+	}
+
+	/**
+	 *  Sets the duration of the movie.
+	 * @param duration the duration to set.
+	 */
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 *  Gets the title of the movie.
+	 * @return the title of the movie.
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/** 
+	 *  Sets the title of the movie.
+	 * @param title the title of the movie.
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 *  Gets the year the movie was published.
+	 * @return the year the movie was published.
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 *  Sets the year the movie was published.
+	 * @param year The year the movie was published. 
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/**
+	 *  Returns the movie object as a formatted String.
+	 * @return The movie object represented as a string.
+	 */
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", duration=" + duration + ", title=" + title + ", year=" + year + "]";
+	}
 }
